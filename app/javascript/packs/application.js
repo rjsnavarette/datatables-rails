4169ea4654854a8933199d373d7738d95c7c31aa
Raven <rhold.navarette@kodakollectiv.com>
2021-03-15 10:18:53 +0800

    Imported JQuery global and require DataTables in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('datatables.net-bs4')
+
+import $ from 'jquery';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+global.$ = jQuery;
 
 Rails.start()
 Turbolinks.start()
